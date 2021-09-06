f0a5fefe11b2c9d56d86847c2bd3cb668f1ae8e9
Adrian Lujan <Adrian Lujan@LAPTOP-GM45TMSI>
2021-09-06 14:49:24 -0500

    Agregar archivos de proyecto.

---
+++ Lab03/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿
+namespace Lab03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtServidor = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBaseDatos = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textUsuario = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textPassword = new System.Windows.Forms.TextBox();
+            this.chkAutenticacion = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnDesconectar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Servidor";
+            // 
+            // txtServidor
+            // 
+            this.txtServidor.AccessibleName = "txtServidor";
+            this.txtServidor.Location = new System.Drawing.Point(58, 63);
+            this.txtServidor.Name = "txtServidor";
+            this.txtServidor.Size = new System.Drawing.Size(184, 22);
+            this.txtServidor.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Base de Datos";
+            // 
+            // textBaseDatos
+            // 
+            this.textBaseDatos.AccessibleName = "txtBaseDatos";
+            this.textBaseDatos.Location = new System.Drawing.Point(58, 143);
+            this.textBaseDatos.Name = "textBaseDatos";
+            this.textBaseDatos.Size = new System.Drawing.Size(184, 22);
+            this.textBaseDatos.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(321, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Usuario";
+            // 
+            // textUsuario
+            // 
+            this.textUsuario.AccessibleName = "";
+            this.textUsuario.Enabled = false;
+            this.textUsuario.Location = new System.Drawing.Point(324, 63);
+            this.textUsuario.Name = "textUsuario";
+            this.textUsuario.Size = new System.Drawing.Size(195, 22);
+            this.textUsuario.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AccessibleName = "";
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(324, 109);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Contraseña";
+            // 
+            // textPassword
+            // 
+            this.textPassword.AccessibleName = "txtPassword";
+            this.textPassword.Enabled = false;
+            this.textPassword.Location = new System.Drawing.Point(324, 142);
+            this.textPassword.Name = "textPassword";
+            this.textPassword.Size = new System.Drawing.Size(195, 22);
+            this.textPassword.TabIndex = 7;
+            // 
+            // chkAutenticacion
+            // 
+            this.chkAutenticacion.AccessibleName = "chkAutenticacion";
+            this.chkAutenticacion.AutoSize = true;
+            this.chkAutenticacion.Checked = true;
+            this.chkAutenticacion.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkAutenticacion.Location = new System.Drawing.Point(58, 201);
+            this.chkAutenticacion.Name = "chkAutenticacion";
+            this.chkAutenticacion.Size = new System.Drawing.Size(179, 21);
+            this.chkAutenticacion.TabIndex = 8;
+            this.chkAutenticacion.Text = "Autenticación integrada";
+            this.chkAutenticacion.UseVisualStyleBackColor = true;
+            this.chkAutenticacion.CheckedChanged += new System.EventHandler(this.chkAutenticacion_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.AccessibleName = "btnConectar";
+            this.button1.Location = new System.Drawing.Point(154, 266);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Conectar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.AccessibleName = "btnEstado";
+            this.button2.Location = new System.Drawing.Point(261, 266);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Estado";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // btnDesconectar
+            // 
+            this.btnDesconectar.AccessibleName = "btnDesconectar";
+            this.btnDesconectar.Enabled = false;
+            this.btnDesconectar.Location = new System.Drawing.Point(365, 266);
+            this.btnDesconectar.Name = "btnDesconectar";
+            this.btnDesconectar.Size = new System.Drawing.Size(118, 23);
+            this.btnDesconectar.TabIndex = 11;
+            this.btnDesconectar.Text = "Desconectar";
+            this.btnDesconectar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDesconectar);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chkAutenticacion);
+            this.Controls.Add(this.textPassword);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textUsuario);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBaseDatos);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtServidor);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtServidor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBaseDatos;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textUsuario;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textPassword;
+        private System.Windows.Forms.CheckBox chkAutenticacion;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnDesconectar;
+    }
+}
+
