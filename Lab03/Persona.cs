79edfd6b6eb1022a5274bc5ebe4d9142aede70dc
Adrian Lujan <Adrian Lujan@LAPTOP-GM45TMSI>
2021-09-12 04:51:44 -0500

    Cambios de Lab04

--- Lab03/Persona.cs
+++ Lab03/Persona.cs
@@ -27,7 +27,7 @@ namespace Lab03
             conn.Open();
             if (conn.State == ConnectionState.Open)
             {
-                String sql = "select * from tbl_usuario";
+                String sql = "select * from Person";
                 SqlCommand cmd = new SqlCommand(sql, conn);
                 SqlDataReader reader = cmd.ExecuteReader();
 
@@ -41,5 +41,38 @@ namespace Lab03
                 MessageBox.Show("La conexión esta cerrada");
             }
         }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+
+            if (conn.State == ConnectionState.Open)
+            {
+                String FirstName = txtNombre.Text;
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "BuscarPersonaNombre";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Connection = conn;
+
+                SqlParameter param = new SqlParameter();
+                param.ParameterName = "@FirstName";
+                param.SqlDbType = SqlDbType.NVarChar;
+                param.Value = FirstName;
+
+                cmd.Parameters.Add(param);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                DataTable dt = new DataTable();
+                dt.Load(reader);
+                dgvListado.DataSource = dt;
+                dgvListado.Refresh();
+            }
+            else
+            {
+                MessageBox.Show("La conexion esta cerrada");
+            }
+        }
+
+
     }
 }
